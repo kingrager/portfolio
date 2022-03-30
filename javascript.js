6a844c2bd7a73041ced2ee3ba39b35d6219a6951
kingrager <100423786+kingrager@users.noreply.github.com>
2022-03-30 13:28:36 +0100

    php

---
+++ javascript.js
@@ -0,0 +1,143 @@
+var a = document.getElementsByClassName("navbar");
+for(var elem in a){
+a[elem].style.color= "inherit";
+}
+
+var a = document.getElementsByClassName("navbar-brand");
+for(var elem in a){
+a[elem].style.fontWeight= "bold";
+]
+
+var a = document.getElementsByClassName("nav-item");
+for(var elem in a){
+a[elem].style.fontWeight= "bold";
+]
+
+var a = document.getElementsByClassName("pic");
+for(var elem in a){
+a[elem].style.width= "320px";
+a[elem].style.height= "320px";
+a[elem].style.marginTop= "30px";
+a[elem].style.marginBottom= "30px";
+}
+
+var a = document.getElementsByClassName("htext");
+for(var elem in a){
+a[elem].style.fontWeight= "bold";
+a[elem].style.textAlign= "center";
+a[elem].style.color= "#ffffff";
+a[elem].style.marginBottom= "0px";
+}
+
+var a = document.getElementsByClassName("hptext");
+for(var elem in a){
+a[elem].style.fontWeight= "bold";
+a[elem].style.textAlign= "center";
+a[elem].style.fontSize= "20px";
+a[elem].style.marginBottom= "40px";
+a[elem].style.marginTop= "20px";
+a[elem].style.color= "#ffffff";
+}
+
+var a = document.getElementsByClassName("about");
+for(var elem in a){
+a[elem].style.fontWeight= "bold";
+a[elem].style.textAlign= "center";
+a[elem].style.marginTop= "60px";
+a[elem].style.marginBottom= "50px";
+}
+
+var a = document.getElementsByClassName("torch");
+for(var elem in a){
+a[elem].style.width= "100%";
+a[elem].style.height= "700px";
+}
+
+var a = document.getElementsByClassName("gif");
+for(var elem in a){
+a[elem].style.width= "80px";
+a[elem].style.height= "80px";
+a[elem].style.float= "left";
+}
+
+var a = document.getElementsByClassName("para");
+for(var elem in a){
+a[elem].style.fontWeight= "bold";
+a[elem].style.marginBottom= "100px";
+a[elem].style.fontSize= "20px";
+}
+
+var a = document.getElementsByClassName("service");
+for(var elem in a){
+a[elem].style.fontWeight= "bold";
+a[elem].style.textAlign= "center";
+a[elem].style.marginBottom= "50px";
+a[elem].style.marginTop= "60px";
+a[elem].style.color= "#ffffff";
+}
+
+var a = document.getElementsByClassName("card-img");
+for(var elem in a){
+a[elem].style.width= "100px";
+a[elem].style.height= "100px";
+a[elem].style.alignSelf= "center";
+}
+
+var a = document.getElementsByClassName("card-title");
+for(var elem in a){
+a[elem].style.textAlign= "center";
+}
+
+var a = document.getElementsByClassName("card-text");
+for(var elem in a){
+a[elem].style.textAlign= "center";
+a[elem].style.fontWeight= "bold";
+}
+
+var a = document.getElementsByClassName("card-tex");
+for(var elem in a){
+  a[elem].style.textAlign= "center";
+  a[elem].style.fontWeight= "bold";
+a[elem].style.marginBottom= "70px";
+}
+
+var a = document.getElementsByClassName("card");
+for(var elem in a){
+a[elem].style.alignSelf= "center";
+a[elem].style.marginBottom= "60px";
+}
+
+var a = document.getElementsByClassName("testimony");
+for(var elem in a){
+a[elem].style.fontWeight= "bold";
+a[elem].style.textAlign= "center";
+a[elem].style.color= "#ffffff";
+}
+
+var a = document.getElementsByClassName("d-block");
+for(var elem in a){
+a[elem].style.height= "100px";
+a[elem].style.alignSelf= "center";
+}
+
+var a = document.getElementsByClassName("name");
+for(var elem in a){
+a[elem].style.fontWeight= "bold";
+a[elem].style.fontStyle= "italic";
+a[elem].style.fontSize= "20px";
+a[elem].style.color= "#ffffff";
+}
+
+var a = document.getElementsByClassName("card-img-top");
+for(var elem in a){
+a[elem].style.width= "40%";
+a[elem].style.height= "40%";
+a[elem].style.alignSelf= "center";
+}
+
+var a = document.getElementsByClassName("last");
+for(var elem in a){
+a[elem].style.color= "#ffffff";
+a[elem].style.fontWeight= "bold";
+a[elem].style.textAlign= "center";
+}
